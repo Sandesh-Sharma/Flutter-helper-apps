51b708553b832b4918e6dff4cb665b6813f555d1
Bikram Parajuli <39263700+Parajulibkrm@users.noreply.github.com>
2021-10-24 15:22:57 +0545

    Rename App title in Main.js

--- bloc_app/lib/main.dart
+++ bloc_app/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Employee App',
+      title: 'Company Management Application',
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
