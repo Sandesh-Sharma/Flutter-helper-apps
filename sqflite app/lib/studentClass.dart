8328eeb75e32741b9ad086b4e6066546629e1ec2
Bigyan Timislina <42671917+bigyanic@users.noreply.github.com>
2021-10-27 15:58:02 +0545

    updated studentClass

--- sqflite app/lib/studentClass.dart
+++ sqflite app/lib/studentClass.dart
@@ -1,4 +1,9 @@
 class Student {
-  
-  
+  int id;
+  String name;
+  String course;
+  Student({@required this.name, @required this.course, this.id});
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'course': course};
+  }
 }
